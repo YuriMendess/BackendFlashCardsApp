e9358ba9ce0d265690e270ae9a30463dbacee2be
YuriMendess <contato.yurimendess@gmail.com>
2023-01-10 17:38:12 -0300

    :sparkles: feat: add UserContext

---
+++ Models/UserContext.cs
@@ -0,0 +1,14 @@
+using BackendFlashCardsApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models;
+
+public class UserContext : DbContext
+{
+    public UserContext(DbContextOptions<UserContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; } = null!;
+}
