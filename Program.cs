ced61281b8cc9082c838517e9d4577aed8e9e78a
YuriMendess <contato.yurimendess@gmail.com>
2023-01-10 17:32:56 -0300

    :sparkles: feat: add UserContext

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<UserContext>(opt => opt.UseInMemoryDatabase("Users"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
